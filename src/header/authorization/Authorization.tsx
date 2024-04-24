4c34a8d666b650e5a0c072599f4cf63a66cdc440
Aboltys754 <warwar754@mail.ru>
2024-04-24 10:50:24 +0500

    stop front

--- src/header/authorization/Authorization.tsx
+++ src/header/authorization/Authorization.tsx
@@ -3,6 +3,21 @@ import style from "./styles.module.css"
 
 export default function authorization () {
     return (
-        <div className={style.root}>3</div>
+        <div className={style.root}>
+            <form action="" className={style.form_authorization}>
+                <div className={style.form_input}>
+                    <label htmlFor="login">Логин</label>
+                    <input type="text" name="login" id="login" />
+                </div>
+                <div className={style.form_input}>
+                    <label htmlFor="password">Пароль</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div className={style.input_button}>
+                    <input type="submit" value="Войти"/>
+                </div>
+
+            </form>
+        </div>
     )
 }
